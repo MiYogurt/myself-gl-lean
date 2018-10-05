917afa22bdac1474748c9b11aa8268c61d55b1c6
ChenMingMing <belovedyogurt@gmail.com>
2018-10-05 21:44:22 +0800

    🚄 openGL 窗口小程序

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,15 +1,63 @@
-//
-//  main.cpp
-//  opengl
-//
-//  Created by 优狗 on 2018/10/5.
-//  Copyright © 2018 yugo. All rights reserved.
-//
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+    
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Lean OpenGL", NULL, NULL);
+    
+    if (window == NULL)
+    {
+        std::cout << "fail to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Faild to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, 800, 600);
+    
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
